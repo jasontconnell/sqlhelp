9375c25243c8cfb67dac36231d0dee13e01bccfa
Jason Connell <jconnell@delphicdigital.com>
2018-04-29 10:26:45 -0400

    Experiment...

--- rs.go
+++ rs.go
@@ -44,6 +44,44 @@ func GetResultsChannel(db *sql.DB, query string, results chan map[string]interfa
 	return nil
 }
 
+func GetResultsChannelExp(db *sql.DB, query string, results chan map[string]interface{}) error {
+	rows, eerr := db.Query(query)
+	if eerr != nil {
+		return eerr
+	}
+
+	cols, cerr := rows.Columns()
+	if cerr != nil {
+		return cerr
+	}
+
+	vals := make([]interface{}, len(cols))
+	for i := 0; i < len(cols); i++ {
+		vals[i] = new(interface{})
+	}
+
+	go func() {
+		defer rows.Close()
+		i := 0
+		for rows.Next() {
+			scerr := rows.Scan(vals...)
+
+			if scerr != nil {
+				panic(scerr)
+			}
+
+			valmap := make(map[string]interface{})
+			for i, col := range cols {
+				valmap[col] = *(vals[i].(*interface{}))
+			}
+			results <- valmap
+			i++
+		}
+	}()
+
+	return nil
+}
+
 func GetResultSet(db *sql.DB, query string) ([]map[string]interface{}, error) {
 	ret := []map[string]interface{}{}
 
